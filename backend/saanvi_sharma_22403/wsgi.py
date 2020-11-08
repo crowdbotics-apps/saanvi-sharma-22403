386e56083961bd551ac27d1f8eedf101a792f4b0
Crowdbotics <team@crowdbotics.com>
2020-11-08 16:50:53 +0000

    Update models

--- backend/saanvi_sharma_22403/wsgi.py
+++ backend/saanvi_sharma_22403/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'saanvi_sharma_22403.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "saanvi_sharma_22403.settings")
 
 application = get_wsgi_application()
